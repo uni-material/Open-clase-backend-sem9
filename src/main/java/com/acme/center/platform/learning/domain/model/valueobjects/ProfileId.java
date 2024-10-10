8f146721a67407fbf6c56cdf2faade8cf5ed8e17
Ro <u202213765@upc.edu.pe>
2024-10-10 15:49:56 -0500

    feat: added profile id.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/valueobjects/ProfileId.java
@@ -0,0 +1,12 @@
+package com.acme.center.platform.learning.domain.model.valueobjects;
+
+public record ProfileId(Long profileId) {
+    public ProfileId{
+        if (profileId < 0)
+            throw new IllegalArgumentException("Profile profileId cannot be negative.");
+    }
+
+    public ProfileId(){
+        this(0L);
+    }
+}
